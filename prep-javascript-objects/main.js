ed1dc62de6bec3e1ff3e7780947a43abb4a15d86
Ignacio Dominguez <idominguezmoral@gmail.com>
2022-11-22 21:54:33 +0000

    completed prep-javascript-object exercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,14 @@
+var person = {
+  firstName: 'ignacio ',
+  lastName: 'dominguez',
+  hobby: 'gaming',
+  job: 'streamer',
+  previousJob: 'monogamist'
+};
+var fullname = person.firstName + person.lastName;
+
+console.log(person);
+console.log('The persons name is:', fullname);
+console.log('The person currently works as a:', person.job);
+console.log('The persons previous job was:', person.previousJob);
+console.log('The complete person object:', person);
