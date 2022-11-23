7d056846ecafae5ccb198117cf45fa2e9e3c3b31
Ignacio Dominguez <idominguezmoral@gmail.com>
2022-11-23 01:26:42 +0000

    changed const length to languages.length.

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -7,7 +7,7 @@ console.log('The first element of the array is', firstElement[0]);
 const thirdElement = languages;
 console.log('The 3rd element of the array is', thirdElement[2]);
 
-const length = 6;
+const length = languages.length;
 console.log('The length of the array is', length);
 
 const lastElement = languages;
